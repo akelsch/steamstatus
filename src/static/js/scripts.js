95224dd016a729a63da613cd24771a95948f187c
Arthur Kelsch <akelsch@users.noreply.github.com>
2019-05-05 04:19:10 +0200

    Refactor javascript

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -1,115 +1,93 @@
-// Start the loop once the document is loaded
-document.addEventListener("DOMContentLoaded", startLoop);
+const COUNTDOWN_DURATION = 45;
 
-/**
- * Calls fetchUpdate() every x seconds.
- */
-function startLoop() {
-    const x = 45;
+// Start update loop once the document is loaded
+document.addEventListener("DOMContentLoaded", () => updateData().then(countdown));
 
-    fetchUpdate()
-        .then(function() {
-            let secondCount = 0;
-
-            const loop = setInterval(function() {
-                secondCount++;
-                let remainingTime = x - secondCount;
-
-                document.querySelector("#countdown").innerHTML = remainingTime;
-
-                if (remainingTime <= 0) {
-                    clearInterval(loop);
-                    startLoop();
-                }
-            }, 1000);
-        }).catch(function(error) {
-            // Reset the countdown
-            document.querySelector("#countdown").innerHTML = x;
-        });
+async function updateData() {
+    try {
+        const response = await fetch("status.json");
+        const data = await response.json();
+        return handleData(data);
+    } catch (error) {
+        return handleError(error);
+    }
 }
 
-/**
- * Runs a single JSON fetch and updates the document.
- */
-async function fetchUpdate() {
-    return fetch("status.json")
-        .then(function(response) {
-            if (!response.ok) {
-                // Pass HTTP status code in case of failure
-                throw Error(response.status);
-            }
+function countdown() {
+    let seconds = COUNTDOWN_DURATION;
 
-            return response.json();
-        }).then(function(json) {
-            // ID: online-users
-            document.querySelector("#online-users").innerHTML = new Intl.NumberFormat().format(json.steam.online);
+    let interval = setInterval(() => {
+        document.getElementById("countdown").textContent = seconds;
 
-            Object.entries(json.steam.services).forEach(([key, value]) => {
-                if (value === 200) {
-                    value = "online";
-                } else if (!isNaN(value)) {
-                    value = "HTTP Status Code " + value;
-                }
+        if (--seconds < 0) {
+            seconds = COUNTDOWN_DURATION;
+            updateData();
+        }
+    }, 1000);
+}
 
-                // IDs: store-status, community-status, api-status
-                key += "-status";
+function handleData(data) {
+    // Online Users
+    let onlineUsers = new Intl.NumberFormat().format(data.steam.online)
+    document.getElementById("online-users").textContent = onlineUsers;
 
-                document.querySelector("#" + key).innerHTML = value;
-            });
+    // Services I
+    Object.entries(data.steam.services).forEach(([key, value]) => {
+        key += "-status";
 
-            Object.entries(json.csgo.services).forEach(([key, value]) => {
-                // IDs: sessions-logon, player-inventories, matchmaking-scheduler
-                key = key.replace(/_/g, "-");
+        if (value === 200) {
+            value = "online";
+        } else if (!isNaN(value)) {
+            value = "HTTP Status Code " + value;
+        }
 
-                document.querySelector("#" + key).innerHTML = value;
-            });
+        document.getElementById(key).textContent = value;
+    });
 
-            Object.entries(json.csgo.servers).forEach(([key, value]) => {
-                // IDs: australia, brazil, chile, china-guangzhou...
-                key = key.toLowerCase().replace(/ /g, "-");
+    // Services II
+    Object.entries(data.csgo.services).forEach(([key, value]) => {
+        key = key.replace(/_/g, "-");
+        document.getElementById(key).textContent = value;
+    });
 
-                document.querySelector("#" + key).innerHTML = value;
-            });
-        }).then(function() {
-            // Keywords to color the statuses differently
-            const goodStatus = ["idle", "low", "normal", "online"];
-            const okayStatus = ["delayed", "medium"];
-            const badStatus = ["high", "offline"];
+    // CS:GO Servers
+    Object.entries(data.csgo.servers).forEach(([key, value]) => {
+        key = key.toLowerCase().replace(/ /g, "-");
+        document.getElementById(key).textContent = value;
+    });
 
-            // Color palette
-            const green = "#6C9541";
-            const blue = "#53A4C4";
-            const red = "#F44336";
+    highlightData();
+}
 
-            // Set color for every status class member
-            document.querySelectorAll(".status").forEach(function(status) {
-                const statusText = status.innerHTML;
+function handleError(error) {
+    let serverMessage = document.getElementById("server-message");
+    serverMessage.textContent = "Oops, something went wrong. Is Flask up and running?";
+    serverMessage.removeAttribute("style");
 
-                if (goodStatus.includes(statusText)) {
-                    status.style.color = green;
-                } else if (okayStatus.includes(statusText)) {
-                    status.style.color = blue;
-                } else if (badStatus.includes(statusText)) {
-                    status.style.color = red;
-                }
-            });
-        }).catch(function(error) {
-            console.log(error);
+    // TODO Stop countdown loop
+}
 
-            const status = error.message;
+function highlightData() {
+    // Color palette
+    const green = "#6C9541";
+    const blue = "#53A4C4";
+    const red = "#F44336";
 
-            let errmsg;
-            if (!isNaN(status)) {
-                errmsg = "HTTP Status Code " + status + ": Check Flask for more details!";
-            } else {
-                errmsg = "Oops, something went wrong. Is Flask up and running?";
-            }
+    // Keywords to color statuses differently
+    const goodStatus = ["idle", "low", "normal", "online"];
+    const okayStatus = ["delayed", "medium"];
+    const badStatus = ["high", "offline"];
 
-            const servermsg = document.querySelector("#servermsg");
-            servermsg.innerHTML = errmsg;
-            servermsg.removeAttribute("style");
+    // Set color for every status class member
+    document.querySelectorAll(".status").forEach(status => {
+        const statusText = status.textContent;
 
-            // Stop the loop
-            return Promise.reject();
-        });
+        if (goodStatus.includes(statusText)) {
+            status.style.color = green;
+        } else if (okayStatus.includes(statusText)) {
+            status.style.color = blue;
+        } else if (badStatus.includes(statusText)) {
+            status.style.color = red;
+        }
+    });
 }
