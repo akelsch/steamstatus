9b63c970d7a75239cfb8e085f210bf3fa905f0d0
Arthur Kelsch <akelsch@users.noreply.github.com>
2021-11-14 16:26:44 +0100

    Make use of string interpolation

--- steamstatus/model.py
+++ steamstatus/model.py
@@ -9,19 +9,19 @@ class Status(db.Model):
     data = db.Column(db.JSON, nullable=False)
 
     def __repr__(self):
-        return "<Status(id={}, timestamp={})>".format(self.id, self.timestamp)
+        return f"<Status(id={self.id}, timestamp={self.timestamp})>"
 
 
 class Flag():
-    def __init__(self, name: str, img: str, plural: bool = False):
+    def __init__(self, name: str, code: str, plural: bool = False):
         self.name = name
-        self.img = "img/flags/{}.gif".format(img)
+        self.img = f"img/flags/{code}.gif"
         self.plural = plural
 
     def __repr__(self):
         if self.plural:
-            return "Flag of the {}".format(self.name)
-        return "Flag of {}".format(self.name)
+            return f"Flag of the {self.name}"
+        return f"Flag of {self.name}"
 
 
 class Region():
